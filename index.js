7cbd7122fc58a9c6ee66a4de0f6452417657cfb8
aditya9129 <adityagg9129@gmail.com>
2024-09-09 11:18:42 +0530

    CORS FIXED

--- index.js
+++ index.js
@@ -118,7 +118,7 @@ app.post("/login", async (req, res) => {
         httpOnly: true,
         secure: process.env.NODE_ENV === 'production',
         sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
-        domain: process.env.NODE_ENV === 'production' ? '.your-domain.com' : 'localhost',
+        domain: process.env.NODE_ENV === 'production' ? 'https://booking-app-backend-asvc.onrender.com' : 'localhost',
         path: '/',
         maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
       }).json({ name: user.name, email: user.email });
